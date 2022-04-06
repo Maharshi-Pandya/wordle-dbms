83ea6db808332099c1885aba1ef725898ce22970
Maharshi-Pandya <maharshi.pandya2002@gmail.com>
2022-04-06 13:10:13 +0530

    Check word existence in tables

--- utils/DB.py
+++ utils/DB.py
@@ -1,14 +1,13 @@
 # connect to the database 'wordle_dbms'
 
-import sys
 import psycopg2
-
 from psycopg2 import Error
 
 # custom imports
 from errors import handle_error
 
 
+# Try and connect to the wordle database
 def connect_to_wordle(_user, _password, _database) -> list:
     # First element is the connection, Second is the error
     # If connection is successful, we return [conn, None]
@@ -32,18 +31,43 @@ def connect_to_wordle(_user, _password, _database) -> list:
     return retList
 
 
+# Check if a word exists in a table
+def check_word_exists(conn, table, word) -> tuple:
+    word_id, word_content = None, None
+    
+    try:
+        with conn.cursor() as cur:
+            check_word_query = f"SELECT id, content FROM {table} WHERE content=\'{word}\';"
+            
+            # Execute and fetch
+            cur.execute(check_word_query)
+            cw_tuple = cur.fetchone()
+            
+            if cw_tuple is not None:
+                word_id, word_content = cw_tuple
+        
+    except (Exception, Error) as error:
+        handle_error(error)
+
+    return (word_id, word_content)
+
+
 # Fetch the last entry's id from a table
 def fetch_last_id(conn, table) -> int:
     last_id = None
     
-    with conn.cursor() as cur:
-        last_id_query = f"SELECT max(id) FROM {table};"
-        
-        # Execute and fetch
-        cur.execute(last_id_query)
-        li_tuple = cur.fetchone()
-        
-        last_id = li_tuple[0]
+    try:
+        with conn.cursor() as cur:
+            last_id_query = f"SELECT max(id) FROM {table};"
+            
+            # Execute and fetch
+            cur.execute(last_id_query)
+            li_tuple = cur.fetchone()
+            
+            last_id = li_tuple[0]
+
+    except (Exception, Error) as error:
+        handle_error(error)
         
     return last_id        
 
@@ -52,20 +76,24 @@ def fetch_last_id(conn, table) -> int:
 def insert_data(conn, table, datafile) -> int:
     row_id = 1
     
-    with open(datafile) as file:
-        for word in file:
-            # word is read
-            word = word.strip()
-            
-            with conn.cursor() as cur:
-                insert_query = f"INSERT INTO {table} (id, content) VALUES ({row_id}, \'{word}\');"
+    try:
+        with open(datafile) as file:
+            for word in file:
+                # word is read
+                word = word.strip()
                 
-                # Execute the query and commit the changes to DB
-                cur.execute(insert_query)
-                conn.commit()
-        
-            row_id += 1
+                with conn.cursor() as cur:
+                    insert_query = f"INSERT INTO {table} (id, content) VALUES ({row_id}, \'{word}\');"
+                    
+                    # Execute the query and commit the changes to DB
+                    cur.execute(insert_query)
+                    conn.commit()
             
+                row_id += 1
+    
+    except (Exception, Error) as error:
+        handle_error(error)
+                
     return row_id
 
 
@@ -97,3 +125,6 @@ handle_error(err)
 
 last_id = fetch_last_id(conn, "allowed")
 print("Last inserted id is:", last_id)
+
+check_word = check_word_exists(conn, "allowed", "abbas")
+print(check_word)
